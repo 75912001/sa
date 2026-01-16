28a6ee25906bd7c68c8063d5db2d79081953fc70
menglingchao <75912001@qq.com>
2026-01-16 10:36:20 +0800

    [feat]创建

---
+++ scripts/Animation.gd
@@ -0,0 +1,26 @@
+extends Node
+
+class_name AnimationMgr
+
+@export var animation_tree_path: NodePath
+
+var animation_tree: AnimationTree
+var state_machine: AnimationNodeStateMachinePlayback
+
+var current_animation_name := "" # 当前动画
+
+func _ready() -> void:
+	animation_tree = get_node(animation_tree_path)
+	if animation_tree: # 激活 AnimationTree
+		animation_tree.active = true
+	state_machine = animation_tree.get("parameters/playback")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func play(animation_name: String) -> void:
+	if current_animation_name != animation_name:
+		current_animation_name = animation_name
+		state_machine.travel(animation_name)
