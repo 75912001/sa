92dd1c2d7733b3428eb43a438ccf539c16b37a77
menglingchao <75912001@qq.com>
2026-01-30 14:25:22 +0800

    [feat]创建

---
+++ Scripts/Equipment/ArmorData.gd
@@ -0,0 +1,9 @@
+class_name ArmorData extends Resource
+
+@export var armor_id: int
+@export_file("*.tscn") var scene_path: String
+@export var socket_name: PbArmor.ArmorType = PbArmor.ArmorType.ArmorType_Head
+
+@export var offset_position: Vector3           # 位置偏移
+@export var offset_rotation_degrees: Vector3   # 旋转（欧拉角，度数）
+@export var offset_scale: Vector3 = Vector3.ONE  # 缩放
