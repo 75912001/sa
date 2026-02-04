9e02d86e8702c4ad80709ba0c3c857b26586f466
menglingchao <75912001@qq.com>
2026-02-04 12:26:45 +0800

    [feat]创建

--- Scripts/Equipment/Armor/Helmet.gd
+++ Scripts/Equipment/Armor/Helmet.gd
@@ -11,11 +11,12 @@ extends Node3D
 func _ready() -> void:
 	assert(model_root != null, "[配置错误] 场景 '%s' 的 model_root 未赋值！请在编辑器中指定" % name)
 
-# 虚函数
+# 装备
 func on_equip() -> void:
 	# 可以在这里处理头盔特有的逻辑，比如隐藏角色的头发
 	print("Helmet: on_equip - %s" % name)
 
+# 卸下
 func on_unequip() -> void:
 	# 恢复头发显示等
 	print("Helmet: on_unequip - %s" % name)
