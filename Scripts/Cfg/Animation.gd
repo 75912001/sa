b011798319138a5fae787976d7749b08b11479b2
menglingchao <75912001@qq.com>
2026-02-06 11:47:49 +0800

    [feat]创建

--- Scripts/Cfg/Animation.gd
+++ Scripts/Cfg/Animation.gd
@@ -14,6 +14,7 @@ class CfgAnimationLibrary extends RefCounted:
 
 # --- 缓存数据 ---
 var libraries: Dictionary = {}  # library_name -> CfgAnimationLibrary
+var godot_libraries_cache: Dictionary = {} # config_name -> Dictionary[category_name, AnimationLibrary]
 
 # 加载配置
 func load(path: String) -> void:
@@ -59,6 +60,39 @@ func check() -> void:
 func assemble() -> void:
 	pass
 
-# 获取动画库
+# 获取动画库数据
 func get_library(name: String) -> CfgAnimationLibrary:
 	return libraries.get(name, null)
+
+# 获取Godot动画库资源字典
+# 返回: Dictionary[String, AnimationLibrary]  CategoryName -> AnimationLibrary
+func get_animation_libraries_map(name: String) -> Dictionary:
+	if godot_libraries_cache.has(name):
+		return godot_libraries_cache[name]
+		
+	var entry = get_library(name)
+	assert(entry, "未找到动画库: %s" % name)
+		
+	var libs_map = {}
+	
+	for category_name in entry.categories:
+		var lib_for_category = AnimationLibrary.new()
+		var animation_list: Array = entry.categories[category_name]
+		
+		for anim_name in animation_list:
+			var full_path = entry.base_path + category_name + "/" + anim_name + ".res"
+			if ResourceLoader.exists(full_path):
+				var anim_res = load(full_path)
+				if anim_res is Animation:
+					# 直接使用 anim_name，因为这个库会被命名为 category_name
+					# 最终效果是: category_name/anim_name
+					lib_for_category.add_animation(anim_name, anim_res)
+				else:
+					assert(false, "资源不是动画: %s" % full_path)
+			else:
+				assert(false, "动画文件丢失(运行时): %s" % full_path)
+		
+		libs_map[category_name] = lib_for_category
+	
+	godot_libraries_cache[name] = libs_map
+	return libs_map
